54d0cd8a0f70d1250c8eb93739432ccd9cc2fd59
Brendan Tronconi <brendan.tronconi@gmail.com>
2015-01-31 14:17:05 -0500

    trying to get a game up, not working but want to see if audio will load and play

--- js/preloader.js
+++ js/preloader.js
@@ -13,10 +13,10 @@ BasicGame.Preloader.prototype = {
   preload: function () {
 
     //  Show the loading progress bar asset we loaded in boot.js
-    this.stage.backgroundColor = '#2d2d2d';
+    this.stage.backgroundColor = '#44be9b';
 
     this.preloadBar = this.add.sprite(this.game.width / 2 - 100, this.game.height / 2, 'preloaderBar');
-    this.add.text(this.game.width / 2, this.game.height / 2 - 30, "Loading...", { font: "32px monospace", fill: "#fff" }).anchor.setTo(0.5, 0.5);
+    this.add.text(this.game.width / 2, this.game.height / 2 - 30, "Loading...", { font: "32px monospace", fill: "#c347ad" }).anchor.setTo(0.5, 0.5);
 
     //  This sets the preloadBar sprite as a loader sprite.
     //  What that does is automatically crop the sprite from 0 to full-width
@@ -25,20 +25,17 @@ BasicGame.Preloader.prototype = {
 
     //  Here we load the rest of the assets our game needs.
     this.load.image('titlepage', 'assets/titlepage.png');
-    this.load.image('sea', 'assets/sea.png');
-    this.load.image('bullet', 'assets/bullet.png');
-    this.load.image('enemyBullet', 'assets/enemy-bullet.png');
-    this.load.image('powerup1', 'assets/powerup1.png');
-    this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
-    this.load.spritesheet('whiteEnemy', 'assets/shooting-enemy.png', 32, 32);
-    this.load.spritesheet('boss', 'assets/boss.png', 93, 75);
-    this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
-    this.load.spritesheet('player', 'assets/player.png', 64, 64);
-    this.load.audio('explosion', ['assets/explosion.ogg', 'assets/explosion.wav']);
-    this.load.audio('playerExplosion', ['assets/player-explosion.ogg', 'assets/player-explosion.wav']);
-    this.load.audio('enemyFire', ['assets/enemy-fire.ogg', 'assets/enemy-fire.wav']);
-    this.load.audio('playerFire', ['assets/player-fire.ogg', 'assets/player-fire.wav']);
-    this.load.audio('powerUp', ['assets/powerup.ogg', 'assets/powerup.wav']);
+    this.load.image('space', 'assets/deep-space.jpg');
+    this.load.image('bullet', 'assets/bullets.png');
+    this.load.image('ship', 'assets/ship.png');
+    this.load.spritesheet('buttonfire', 'assets/button-round-a.png',96, 96);
+    this.load.spritesheet('buttonjump', 'assets/button-round-b.png',96,96);
+	
+ 	  this.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+			
+    this.load.audio('titlemusic', ['assets/StartScreen.ogg', 'assets/StartScreen.wav']);
+    this.load.audio('startgame', ['assets/StartGame.ogg', 'assets/StartGame.wav']);
+ 
     //this.load.audio('titleMusic', ['audio/main_menu.mp3']);
     //  + lots of other required assets here
 
